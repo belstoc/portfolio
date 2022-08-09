ff723cb451c57c750d8c02475e2d331595787e1b
sincsoft <ericvelez@bellstockton.com>
2022-08-08 20:06:55 -0400

    start over

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from './Components/Layouts/Navbar.js'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Navbar from '../src/components/layouts/Navbar'
+import Headshot from "../public/headshot.jpg"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-    <Navbar/>
       <Head>
         <title>Portfolio</title>
         <meta name="description" content="Eric Velez - Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
+      <Navbar />
        
-      
-       
-     
-
-         
-       
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,8 +1,20 @@
+import Link from 'next/link'
+import styles from '../../../styles/Home.module.css'
+
 function Navbar (){
     return (
-        <div className="container">
-            <h1>HEllo </h1>
+      <div className={styles.navbar}>
+        <div className={styles.title}>
+          <h1>ERIC VELEZ</h1>
         </div>
-    )
+        <div className={styles.navList}>
+          <ul className={styles.ul}>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    );
 }
 export default Navbar
